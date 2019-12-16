b811dd1b73f6192e1ba16dd8829f5660da95da2b
Alessandro <sandroales405@gmail.com>
2019-12-16 17:08:22 +0700

    add seeder saham investor

--- app/Http/Controllers/Operator/SahamInvestorController.php
+++ app/Http/Controllers/Operator/SahamInvestorController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Operator;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\SahamInvestor;
 
 class SahamInvestorController extends Controller
 {
     public function index()
     {
-        return view('operator/form_saham.index');
+        $saham = SahamInvestor::select('investor_id','seri_spmpkop','seri_formulir','jumlah_saham','terbilang_saham','jenis_mata_uang','pembayaran_no_rek','pembayaran_nm_rek','pembayaran_nm_bank','no_sk3s_lama','perubahan_k','status_verifikasi');
+        return view('operator/form_saham.index', compact('saham'));
     }
 }
